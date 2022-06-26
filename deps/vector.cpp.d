f3c3c827dea55ab6eab329aa50a7501b5f520e92
Aerok925 <sokolov.maksim.1976@mail.ru>
2022-06-26 19:14:05 +0300

    del d/o files

--- deps/vector.cpp.d
+++
@@ -1,7 +0,0 @@
-objs/vector.cpp.o deps/vector.cpp.d: srcs/vector.cpp \
-  srcs/../incs/vector.hpp incs/reverse_iterator.hpp incs/iterator.hpp \
-  incs/utilities.hpp incs/is_integral.hpp incs/enable_if.hpp \
-  incs/equal.hpp incs/lexicographical_compare.hpp incs/pair.hpp \
-  srcs/../incs/stack.hpp incs/vector.hpp srcs/../incs/BlackRedTree.hpp \
-  incs/TreeIterator.hpp incs/Node.hpp srcs/../incs/map.hpp \
-  incs/BlackRedTree.hpp srcs/../incs/set.hpp
